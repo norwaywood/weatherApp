dbaa670f3889bed9edfe129a28bf9d749da4010f
norwaywood <sejin237.lee@gmail.com>
2026-07-28 12:56:33 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,17 @@
+import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProduction ? "/weatherApp" : "",
+  assetPrefix: isProduction ? "/weatherApp/" : "",
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
